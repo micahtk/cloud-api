18803d6e7ef7c974b68fff892d25de48dd7bb339
Ryan Malone <ryan@alwaysai.co>
2019-11-21 17:38:01 -0800

    wip

--- src/rpc-method-specs/list-models.ts
+++ src/rpc-method-specs/list-models.ts
@@ -1,15 +1,20 @@
 import * as t from 'io-ts';
 import { rpcModelVersionCodec } from '../rpc-model-version';
 
-// const publisherCodec = t.string;
-
-// const args = t.type({ publisher: t.array(publisherCodec) });
-
 export const listModels = {
   description: 'List alwaysAI models',
   argsCodec: t.tuple([
-    t.type({
+    t.partial({
       publisher: t.array(t.string),
+      frameworkType: t.array(t.string),
+      categories: t.array(t.string),
+      powerConsumption: t.string,
+      size: t.string,
+      license: t.array(t.string),
+      inferenceTime: t.string,
+      board: t.string,
+      dataSet: t.array(t.string),
+      osCompatibility: t.array(t.string),
     }),
   ]),
   resultCodec: t.array(rpcModelVersionCodec),
