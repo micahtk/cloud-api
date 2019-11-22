426b2be97d9b8ca879b1ff5944c02f9a8ec9ac92
Ryan Malone <ryan@alwaysai.co>
2019-11-22 14:11:19 -0800

    wip

--- src/rpc-method-specs/list-models.ts
+++ src/rpc-method-specs/list-models.ts
@@ -1,6 +1,17 @@
 import * as t from 'io-ts';
 import { rpcModelVersionCodec } from '../rpc-model-version';
 
+const Range = t.type({
+  rangeMin: t.number,
+  rangeMax: t.number,
+});
+
+const OperatingSystems = t.type({
+  osx: t.boolean,
+  windows: t.boolean,
+  linux: t.boolean,
+});
+
 export const listModels = {
   description: 'List alwaysAI models',
   argsCodec: t.tuple([
@@ -8,13 +19,13 @@ export const listModels = {
       publisher: t.array(t.string),
       frameworkType: t.array(t.string),
       categories: t.array(t.string),
-      powerConsumption: t.string,
-      size: t.string,
+      powerConsumption: Range,
+      size: Range,
       license: t.array(t.string),
-      inferenceTime: t.string,
+      inferenceTime: Range,
       board: t.string,
       dataSet: t.array(t.string),
-      osCompatibility: t.array(t.string),
+      osCompatibility: OperatingSystems,
     }),
   ]),
   resultCodec: t.array(rpcModelVersionCodec),
